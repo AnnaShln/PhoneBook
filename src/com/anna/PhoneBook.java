8bfe3d090dfb5384f3d58489ed7bdc7777f0aa2f
Анна_2 <AnnaShln>
2019-03-29 15:51:47 +0300

    phonebook

--- src/com/anna/PhoneBook.java
+++ src/com/anna/PhoneBook.java
@@ -2,55 +2,65 @@ package com.anna;
 
 import java.util.*;
 
-//Хранит список людей и номеров их телефонов. У каждого человека может быть более одного номера телефона.
-    //Методы: добавление / удаление человека, добавление / удаление телефона для заданного человека,
-    //поиск номера(ов) телефона по заданному имени человека, поиск человека по заданному номеру телефона.
-    //Телефон задаётся последовательностью символов из цифр, +, *, #, -.
-
 public class PhoneBook {
     
     private Map<String, Set<String>> persons = new HashMap<>();
 
     public boolean addPerson(String name, String number) {
-        Set<String> numbers = new HashSet<>();
-        if (number.matches("[\\d+#*+-]+")) numbers.add(number);
-        else return false;
-        persons.put(name, numbers);
-        return persons.containsKey(name);
+        if (persons.containsKey(name) || name == null) return false;
+        else {
+            Set<String> numbers = new HashSet<>();
+            if (number.matches("[\\d+#*-]+")) numbers.add(number);
+            else return false;
+            persons.put(name, numbers);
+            return persons.containsKey(name);
+        }
     }
 
     public boolean deletePerson(String name) {
-        persons.remove(name);
-        return persons.containsKey(name);
+        if (persons.containsKey(name)) {
+            persons.remove(name);
+            return true;
+        }
+        else return false;
     }
 
     public boolean addNumber(String number, String name) {
-        if (number.matches("[\\d+#*+-]+")) {
-            if (persons.containsKey(name))  return persons.get(name).add(number);
-            else return false;
+        if (persons.containsKey(name)) {
+            if (number.matches("[\\d+#*-]+")) {
+                if (persons.containsKey(name)) return persons.get(name).add(number);
+                else return false;
+            } else return false;
         }
         else return false;
     }
 
     public boolean deleteNumber(String number, String name) {
-        if (number.matches("[\\d+#*+-]+")) {
-            if (persons.containsKey(name)) return persons.get(name).remove(number);
+        if (persons.containsKey(name)) {
+            if (number.matches("[\\d+#*-]+")) {
+                for (String value : persons.get(name)) {
+                    if (number.equals(value)) {
+                        persons.get(name).remove(number);
+                        return true;
+                    }
+                    else return false;
+                }
+            }
             else return false;
         }
-        else return false;
+        return false;
     }
 
     public Set searchByName(String name) {
-        return persons.get(name);
+        return persons.getOrDefault(name, null);
     }
 
     public String searchByNumber (String number) {
         String wantedname = null;
-        if (number.matches("[\\d+#*+-]+")) {
+        if (number.matches("[\\d+#*-]+")) {
             for (String key : persons.keySet()) {
-                persons.get(key);
-                for (Object value : persons.get(key)) {
-                    if (number == value) return wantedname = key;
+                for (String value : persons.get(key)) {
+                    if (number.equals(value)) return wantedname = key;
                 }
             }
             return wantedname;
--- tests/com/anna/PhoneBookTest.java
+++ tests/com/anna/PhoneBookTest.java
@@ -1,7 +1,5 @@
 package com.anna;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
-
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,8 +9,20 @@ class PhoneBookTest {
     @org.junit.jupiter.api.Test
     void addPerson() {
         PhoneBook example = new PhoneBook();
-        Boolean actual = example.addPerson("Вера", "+233-2423");
-        assertEquals(true, actual);
+        boolean actual = example.addPerson("Вера", "+233-2423");
+        assertTrue(actual);
+    }
+    @org.junit.jupiter.api.Test
+    void addPerson_NullName() {
+        PhoneBook example = new PhoneBook();
+        boolean actual = example.addPerson(null, "+233-2423");
+        assertFalse(actual);
+    }
+    @org.junit.jupiter.api.Test
+    void addPerson_WrongNumber() {
+        PhoneBook example = new PhoneBook();
+        boolean actual = example.addPerson("Юля", "+233-ssflk");
+        assertFalse(actual);
     }
 
     @org.junit.jupiter.api.Test
@@ -20,8 +30,24 @@ class PhoneBookTest {
         PhoneBook example = new PhoneBook();
         example.addPerson("Вера", "+2343-2423");
         example.addPerson("Лера", "#0983298");
-        Boolean actual = example.deletePerson("Варя");
-        assertEquals(false, actual);
+        boolean actual = example.deletePerson("Вера");
+        assertTrue(actual);
+    }
+    @org.junit.jupiter.api.Test
+    void deletePerson_NoNameInMap() {
+        PhoneBook example = new PhoneBook();
+        example.addPerson("Вера", "+2343-2423");
+        example.addPerson("Лера", "#0983298");
+        boolean actual = example.deletePerson("Катя");
+        assertFalse(actual);
+    }
+    @org.junit.jupiter.api.Test
+    void deletePerson_NullName() {
+        PhoneBook example = new PhoneBook();
+        example.addPerson("Вера", "+2343-2423");
+        example.addPerson("Лера", "#0983298");
+        boolean actual = example.deletePerson(null);
+        assertFalse(actual);
     }
 
     @org.junit.jupiter.api.Test
@@ -29,8 +55,24 @@ class PhoneBookTest {
         PhoneBook example = new PhoneBook();
         example.addPerson("Маша", "293874837");
         example.addPerson("Катя", "09238499339");
+        boolean actual = example.addNumber("85780240", "Маша");
+        assertTrue(actual);
+    }
+    @org.junit.jupiter.api.Test
+    void addNumber_WrongNumber() {
+        PhoneBook example = new PhoneBook();
+        example.addPerson("Маша", "+293874837");
+        example.addPerson("Катя", "#0923-8499339");
+        boolean actual = example.addNumber("!fsdf0240", "Юля");
+        assertFalse(actual);
+    }
+    @org.junit.jupiter.api.Test
+    void addNumber_NoName() {
+        PhoneBook example = new PhoneBook();
+        example.addPerson("Маша", "+293874837");
+        example.addPerson("Катя", "#0923-8499339");
         boolean actual = example.addNumber("85780240", "Юля");
-        assertEquals(false, actual);
+        assertFalse(actual);
     }
 
     @org.junit.jupiter.api.Test
@@ -40,7 +82,34 @@ class PhoneBookTest {
         example.addPerson("Катя", "+09238499339");
         example.addNumber("85780240", "Катя");
         boolean actual = example.deleteNumber("+09238499339", "Катя");
-        assertEquals(true, actual);
+        assertTrue(actual);
+    }
+    @org.junit.jupiter.api.Test
+    void deleteNumber_NoName() {
+        PhoneBook example = new PhoneBook();
+        example.addPerson("Маша", "293874837");
+        example.addPerson("Катя", "+09238499339");
+        example.addNumber("85780240", "Катя");
+        boolean actual = example.deleteNumber("+09238499339", "Варя");
+        assertFalse(actual);
+    }
+    @org.junit.jupiter.api.Test
+    void deleteNumber_WrongNumber() {
+        PhoneBook example = new PhoneBook();
+        example.addPerson("Маша", "293874837");
+        example.addPerson("Катя", "+09238499339");
+        example.addNumber("85780240", "Катя");
+        boolean actual = example.deleteNumber("Hello", "Катя");
+        assertFalse(actual);
+    }
+    @org.junit.jupiter.api.Test
+    void deleteNumber_NoNumber() {
+        PhoneBook example = new PhoneBook();
+        example.addPerson("Маша", "293874837");
+        example.addPerson("Катя", "+09238499339");
+        example.addNumber("85780240", "Катя");
+        boolean actual = example.deleteNumber("+3234-243", "Катя");
+        assertFalse(actual);
     }
 
     @org.junit.jupiter.api.Test
@@ -49,8 +118,20 @@ class PhoneBookTest {
         example.addPerson("Маша", "293874837");
         example.addPerson("Катя", "09238499339");
         example.addNumber("85780240", "Катя");
-        Set actual = example.searchByName("Юля");
-        assertEquals(null, actual);
+        Set actual = example.searchByName("Катя");
+        Set<String> expected = new HashSet<>();
+        expected.add("85780240");
+        expected.add("09238499339");
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void searchByName_NoName() {
+        PhoneBook example = new PhoneBook();
+        example.addPerson("Маша", "293874837");
+        example.addPerson("Катя", "09238499339");
+        example.addNumber("85780240", "Катя");
+        Set actual = example.searchByName(null);
+        assertNull(actual);
     }
 
     @org.junit.jupiter.api.Test
@@ -59,7 +140,16 @@ class PhoneBookTest {
         example.addPerson("Маша", "293874837");
         example.addPerson("Катя", "09238499339");
         example.addNumber("85780240", "Катя");
+        String actual = example.searchByNumber("85780240");
+        assertEquals("Катя",actual);
+    }
+    @org.junit.jupiter.api.Test
+    void searchByNumber_WrongNumber() {
+        PhoneBook example = new PhoneBook();
+        example.addPerson("Маша", "293874837");
+        example.addPerson("Катя", "09238499339");
+        example.addNumber("85780240", "Катя");
         String actual = example.searchByNumber("!85780240");
-        assertEquals(null, actual);
+        assertNull(actual);
     }
 }
